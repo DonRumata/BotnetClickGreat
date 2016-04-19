6b891666362a51a52de03774350e4a870b3b725e
DonRumata <don_kondor@mail.ru>
2016-04-19 10:10:00 +0300

    FirstAriphmetic Switches and converting

--- BotnetClickGreat/Token.cs
+++ BotnetClickGreat/Token.cs
@@ -8,10 +8,10 @@ namespace BotnetClickGreat
 {
     class Token //Базовый класс токена, содержит в себе лишь общие значения для всех токенов.
     {
-        string Data { get; set; } //Текстовое значение слова содержащегося в токене.
-        int ID { get; set; } //ID конкретного токена.
-        public string Number_range;
-        int Row_count { get; set; }
+        protected string Data { get; set; } //Текстовое значение слова содержащегося в токене.
+        protected int ID { get; set; } //ID конкретного токена.
+        protected string Number_range;
+        protected int Row_count { get; set; }
 
         public Token(string data, int First_numR, int Second_numR, int row_counter, int id) //Конструктор базового класса задающий базовые значения извне
         {
@@ -29,14 +29,14 @@ namespace BotnetClickGreat
             this.Number_range = "UNDEFINED";
         }
 
-        public int Number_range_converter(out int Last_value)  //Возвращает в виде Int значений диапазон номеров символов для текущего слова
+        private int Number_range_converter(out int Last_value)  //Возвращает в виде Int значений диапазон номеров символов для текущего слова
         {
             int help_value = Number_range.IndexOf('-');
             Last_value = int.Parse(Number_range.Substring(help_value));
             return int.Parse(Number_range.Substring(0, help_value));
         }
 
-        public void Number_range_former(int First_char_num, int Last_char_num)  //Формирует строку хранящую диапазон номеров символов для текущего слова
+        private void Number_range_former(int First_char_num, int Last_char_num)  //Формирует строку хранящую диапазон номеров символов для текущего слова
         {
             this.Number_range = (First_char_num.ToString() + "-" + Last_char_num.ToString());
         }
@@ -46,9 +46,65 @@ namespace BotnetClickGreat
     {
         public Ariphmetical(string ndata, int nFirst_numR, int nSecond_numR, int nrow_counter, int nid) : base(ndata, nFirst_numR, nSecond_numR, nrow_counter, nid) { }
         public Ariphmetical() : base() { }
-        public string Interpretation_function()
+        public object Interpretation_function(string first_value, string second_value)
         {
-
+            double FValue = Convert.ToDouble(first_value);
+            double SValue = Convert.ToDouble(first_value);
+            if (Data.Length==1)
+            {
+                switch (Data)
+                {
+                    case "+":
+                        break;
+                    case "-":
+                        break;
+                    case "*":
+                        break;
+                    case "/":
+                        break;
+                    case "%":
+                        break;
+                    case "^":
+                        break;
+                }
+            }
+            else
+            {
+                switch (Data)
+                {
+                    case "+=":
+                        break;
+                    case "-=":
+                        break;
+                    case "*=":
+                        break;
+                    case "/=":
+                        break;
+                }
+            }
+           /* switch (Data)
+            {
+                case "+":
+                    break;
+                case "-":
+                    break;
+                case "+=":
+                    break;
+                case: "-=":
+                    break;
+                case "*":
+                    break;
+                case "/":
+                    break;
+                case "*=":
+                    break;
+                case "/=":
+                    break;
+                case "%":
+                    break;
+                case "^":
+                    break;
+            }*/
             return "trulala";
         }
     }
